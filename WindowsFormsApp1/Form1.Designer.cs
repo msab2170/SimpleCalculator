999ac29a2106797c6b374b672ee90097b630228e
User <User@DESKTOP-J7M1PH6>
2023-04-25 15:58:14 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.btnPlus, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnEnter, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn0, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnReset, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnMinus, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnMultiply, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDivide, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 94);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 352);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.AutoSize = true;
+            this.btnPlus.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnPlus.Location = new System.Drawing.Point(585, 267);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(188, 82);
+            this.btnPlus.TabIndex = 15;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.AutoSize = true;
+            this.btnEnter.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnEnter.Location = new System.Drawing.Point(391, 267);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(188, 82);
+            this.btnEnter.TabIndex = 14;
+            this.btnEnter.Text = "=";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.AutoSize = true;
+            this.btn0.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn0.Location = new System.Drawing.Point(197, 267);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(188, 82);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.AutoSize = true;
+            this.btnReset.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnReset.Location = new System.Drawing.Point(3, 267);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(188, 82);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "AC";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.AutoSize = true;
+            this.btnMinus.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnMinus.Location = new System.Drawing.Point(585, 179);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(188, 82);
+            this.btnMinus.TabIndex = 11;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.AutoSize = true;
+            this.btn3.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn3.Location = new System.Drawing.Point(391, 179);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(188, 82);
+            this.btn3.TabIndex = 10;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.AutoSize = true;
+            this.btn2.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn2.Location = new System.Drawing.Point(197, 179);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(188, 82);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.AutoSize = true;
+            this.btn1.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn1.Location = new System.Drawing.Point(3, 179);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(188, 82);
+            this.btn1.TabIndex = 8;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.AutoSize = true;
+            this.btnMultiply.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnMultiply.Location = new System.Drawing.Point(585, 91);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(188, 82);
+            this.btnMultiply.TabIndex = 7;
+            this.btnMultiply.Text = "X";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.AutoSize = true;
+            this.btn6.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn6.Location = new System.Drawing.Point(391, 91);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(188, 82);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.AutoSize = true;
+            this.btn5.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn5.Location = new System.Drawing.Point(197, 91);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(188, 82);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.AutoSize = true;
+            this.btn4.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn4.Location = new System.Drawing.Point(3, 91);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(188, 82);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.AutoSize = true;
+            this.btnDivide.Font = new System.Drawing.Font("굴림", 15F);
+            this.btnDivide.Location = new System.Drawing.Point(585, 3);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(188, 82);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.AutoSize = true;
+            this.btn9.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn9.Location = new System.Drawing.Point(391, 3);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(188, 82);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.AutoSize = true;
+            this.btn8.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn8.Location = new System.Drawing.Point(197, 3);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(188, 82);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.AutoSize = true;
+            this.btn7.Font = new System.Drawing.Font("굴림", 15F);
+            this.btn7.Location = new System.Drawing.Point(3, 3);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(188, 82);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.result.Font = new System.Drawing.Font("굴림", 20F);
+            this.result.Location = new System.Drawing.Point(12, 10);
+            this.result.Name = "result";
+            this.result.Padding = new System.Windows.Forms.Padding(5);
+            this.result.Size = new System.Drawing.Size(773, 70);
+            this.result.TabIndex = 1;
+            this.result.Text = "0";
+            this.result.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 464);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool afterEnter = false;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e) { }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)  { }
+  
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true)  { result.Text = "1"; }      
+            else { result.Text += 1; }
+            afterEnter = false;
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "2"; }
+            else { result.Text += 2; }
+            afterEnter = false;
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "3"; }
+            else { result.Text += 3; }
+            afterEnter = false;
+        }
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "4"; }
+            else { result.Text += 4; }
+            afterEnter = false;
+        }
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "5"; }
+            else { result.Text += 5; }
+            afterEnter = false;
+        }
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "6"; }
+            else { result.Text += 6; }
+            afterEnter = false;
+        }
+   
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "7"; }
+            else { 
+                result.Text += 7; 
+            }
+            afterEnter = false;
+        }
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "8"; }
+            else { result.Text += 8; }
+            afterEnter = false;
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "9"; }
+            else { result.Text += 9; }
+            afterEnter = false;
+        }
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || afterEnter == true) { result.Text = "0"; }
+            else { result.Text += 0; }
+            afterEnter = false;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if(result.Text == "0으로 나눌 수 없습니다.")
+            {
+                result.Text = 0.ToString();
+            }
+            afterEnter = false;
+            if (result.Text.Contains(" "))
+            {
+                btnEnter_Click(sender, e);
+                afterEnter = false;
+            }
+            result.Text += " * ";
+        }
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (result.Text == "0으로 나눌 수 없습니다.")
+            {
+                result.Text = 0.ToString();
+            }
+            afterEnter = false;
+            if (result.Text.Contains(" "))
+            {
+                btnEnter_Click(sender, e);
+                afterEnter = false;
+            }
+            result.Text += " / ";
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (result.Text == "0으로 나눌 수 없습니다.")
+            {
+                result.Text = 0.ToString();
+            }
+            afterEnter = false;
+            if (result.Text.Contains(" "))
+            {
+                btnEnter_Click(sender, e);
+                afterEnter = false;
+            }
+            result.Text += " - ";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (result.Text == "0으로 나눌 수 없습니다.")
+            {
+                result.Text = 0.ToString();
+            }
+            afterEnter = false;
+            if(result.Text.Contains(" "))
+            {
+                btnEnter_Click(sender, e);
+                afterEnter = false;
+            }
+            result.Text += " + ";
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            String temp = result.Text;
+            if (!temp.Contains(" ")) return;
+            String[] arr = temp.Split(' ');
+            int a = Int32.Parse(arr[0]);
+            int b = Int32.Parse(arr[2]);
+            afterEnter = true;
+            if (arr[1] == "*")
+            {
+                result.Text = (a * b).ToString();
+            } else if (arr[1] == "/")
+            {
+                if ( b == 0)
+                {
+                    result.Text = "0으로 나눌 수 없습니다.";
+                    afterEnter = true;
+                } else
+                {
+                    result.Text = (a / b).ToString();
+                }
+                
+            } else if (arr[1] == "-")
+            {
+                result.Text = (a - b).ToString();
+            } else if (arr[1] == "+")
+            {
+                result.Text = (a + b).ToString();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+    }
+}
